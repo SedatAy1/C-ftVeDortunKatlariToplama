0eb63d159d6b0353fa20fcbd3c64482bb4d2928c
Sedat Ay <159232350+SedatAy1@users.noreply.github.com>
2024-02-27 15:43:50 +0300

    CıftVeDortunKatlariToplama
    CıftVeDortunKatlariToplama

---
+++ CıftVeDortunKatlariToplama.java
@@ -0,0 +1,20 @@
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class CıftVeDortunKatlariToplama {
+    public static void main(String[] args) {
+        int n;
+        int toplam =0;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Sayıyı Giriniz : ");
+            n = scanner.nextInt();
+            if (n % 2 == 0 && n % 4 == 0) {
+                toplam += n;
+            }
+        } while (n % 2 == 0);
+        System.out.println("Çift ve 4'ün Katı Olan Sayıların Toplamı: " +toplam);
+        scanner.close();
+
+    }
+}
